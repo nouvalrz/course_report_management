632ae042a2923b587487bbbc4570c10eb7fa802b
Nouval Rizky <nouvalr@gmail.com>
2024-06-07 16:39:09 +0800

    feat. update class

--- app/controllers/admin/master_classes_controller.rb
+++ app/controllers/admin/master_classes_controller.rb
@@ -1,4 +1,5 @@
 class Admin::MasterClassesController < Admin::ApplicationController
+
   def index
     @pagy, @master_classes = pagy(MasterClass.order(:id))
   end
@@ -20,6 +21,28 @@ class Admin::MasterClassesController < Admin::ApplicationController
         render :new, status: :unprocessable_entity
     end
   end
+
+  def edit
+    @students = Student.order(:id)
+    @teachers = Teacher.order(:id)
+    @master_class = MasterClass.find(params[:id])
+  end
+
+  def update
+    @master_class = MasterClass.find(params[:id])
+    @master_class.update(master_class_params)
+
+    if @master_class.valid?
+      redirect_to edit_admin_master_class_path(@master_class), flash: {success: "Class successfully updated"}
+    else
+      @students = Student.order(:id)
+      @teachers = Teacher.order(:id)
+      flash.now[:error] = @master_class.errors.full_messages.to_sentence
+      render :edit, status: :unprocessable_entity
+    end
+  end
+
+  private
 
   def master_class_params
     params.require(:master_class).permit(:name, :teacher_id, :is_active, student_ids: [])
