92c99dcc03fd94f8620fd2cac37fed32e9855c09
Nouval Rizky <nouvalr@gmail.com>
2024-06-21 11:03:49 +0800

    feat. create and update report

--- app/controllers/teacher/reports_controller.rb
+++ app/controllers/teacher/reports_controller.rb
@@ -1,21 +1,52 @@
 class Teacher::ReportsController < Teacher::ApplicationController
+  before_action :set_teacher_and_student, only: [:new, :create, :edit, :update]
+  before_action :set_report, only: [:edit, :update]
 
   def new
-    @teacher = current_user.teacher
-    @student = @teacher.students.find_by(id: params[:student_id])
-
-    return redirect_to teacher_students_path, flash: {error: "Student not found"} if @student.nil?
-
     @course_enrollment = CourseEnrollment.find(params[:course_enrollment_id])
-    @master_class = @course_enrollment.master_class
-    @report = Report.new(student_id: @student.id, course_enrollment_id: @course_enrollment.id, teacher_id: @course_enrollment.master_class.teacher.id)
+    @report = Report.new(student_id: @student.id, course_enrollment_id: @course_enrollment.id, teacher_id: @teacher.id)
     @report.report_details.build
   end
-  def edit
 
+  def create
+    @report = Report.new(report_params.merge(teacher_id: @teacher.id, student_id: @student.id, course_enrollment_id: params[:course_enrollment_id]))
+
+    if @report.save
+      @report.calculateAvgScore
+      redirect_to teacher_student_path(@student), flash: {success: "Report successfully submitted"}
+    else
+      flash.now[:error] = @report.errors.full_messages.to_sentence
+      render :new
+    end
+  end
+
+  def edit
+    @course_enrollment = CourseEnrollment.find(params[:course_enrollment_id])
   end
 
   def update
+    if @report.update(report_params)
+      @report.calculateAvgScore
+      redirect_to teacher_student_path(@student), flash: {success: "Report successfully updated"}
+    else
+      flash.now[:error] = @report.errors.full_messages.to_sentence
+      render :edit
+    end
+  end
 
+  private
+
+  def set_teacher_and_student
+    @teacher = current_user.teacher
+    @student = @teacher.students.find_by(id: params[:student_id])
+    redirect_to teacher_students_path, flash: {error: "Student not found"} if @student.nil?
+  end
+
+  def set_report
+    @report = Report.find(params[:id])
+  end
+
+  def report_params
+    params.require(:report).permit(report_details_attributes: [:id, :report_criterium_id, :description, :score, :_destroy])
   end
 end
--- app/models/report.rb
+++ app/models/report.rb
@@ -6,7 +6,7 @@ class Report < ApplicationRecord
 
   accepts_nested_attributes_for :report_details, allow_destroy: true
 
-  validates :average_score, numericality: {greater_than_or_equal_to: 0, less_than_or_equal_to: 100}
+  validates :average_score, numericality: {greater_than_or_equal_to: 0, less_than_or_equal_to: 100}, allow_nil: true, allow_blank: true
   validates :is_publish, inclusion: { in: [true, false] }
   validates :course_enrollment_id, presence: true
   validates :teacher_id, presence: true
@@ -14,5 +14,10 @@ class Report < ApplicationRecord
 
   def self.report_exist?(course_enrollment_id, student_id)
     Report.find_by(course_enrollment_id: course_enrollment_id, student_id: student_id).present?
+  end
+
+  def calculateAvgScore
+    self.average_score = self.report_details.sum(:score) / self.report_details.count
+    self.save
   end
 end
